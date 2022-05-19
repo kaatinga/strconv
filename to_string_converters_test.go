64dc21741eccb7490fd667d7911ad5797a5e3f2d
Mikhail Gunkov <painter@3lines.club>
2022-05-19 09:57:13 +0200

    one unified for 2string converters

--- to_string_converters_test.go
+++ to_string_converters_test.go
@@ -1,60 +1,43 @@
 package strconv
 
 import (
-	"reflect"
 	"testing"
 )
 
-func TestUint162String(t *testing.T) {
-
+func Test2String(t *testing.T) {
 	tests := []struct {
 		result string
 		num    uint16
 	}{
-		{"199", 199},
-		{"1999", 1999},
-		{"222", 222},
+		{"0", 0},
 		{"1", 1},
-		{"0", 0},
-		{"55555", 55555},
-		{"12345", 12345},
-		{"10000", 10000},
-		{"9999", 9999},
-	}
-
-	// nolint:scopelint
-	for _, tt := range tests {
-		t.Run(tt.result, func(t *testing.T) {
-			got := Uint162String(tt.num)
-			if got != tt.result {
-				t.Errorf("Uint162String() = %v, want %v", got, tt.result)
-			}
-		})
-	}
-}
-
-func TestByte2String(t *testing.T) {
-
-	tests := []struct {
-		result string
-		num    byte
-	}{
-		{"199", 199},
-		{"99", 99},
-		{"100", 100},
-		{"255", 255},
-		{"222", 222},
-		{"0", 0},
 		{"9", 9},
 		{"10", 10},
-		{"1", 1},
+		{"22", 22},
+		{"99", 99},
+		{"100", 100},
+		{"199", 199},
+		{"222", 222},
+		{"255", 255},
+		{"1999", 1999},
+		{"9999", 9999},
+		{"10000", 10000},
+		{"55555", 55555},
+		{"12345", 12345},
 	}
 
 	// nolint:scopelint
 	for _, tt := range tests {
 		t.Run(tt.result, func(t *testing.T) {
-			if got := Byte2String(tt.num); !reflect.DeepEqual(got, tt.result) {
-				t.Errorf("Byte2String() = %v, want %v", got, tt.result)
+			gotUint16 := Uint162String(tt.num)
+			if gotUint16 != tt.result {
+				t.Errorf("Uint162String() = %v, want %v", gotUint16, tt.result)
+			}
+			if tt.num < 256 {
+				gotByte := Byte2String(byte(tt.num))
+				if gotByte != tt.result {
+					t.Errorf("Byte2String() = %v, want %v", gotByte, tt.result)
+				}
 			}
 		})
 	}

ecf96762743b45e0c13ccbc806adf0e8031c9ebc
Mikhail Gunkov <painter@3lines.club>
2022-05-19 09:16:49 +0200

    more tests

--- helpers_test.go
+++ helpers_test.go
@@ -11,7 +11,11 @@ func TestDeleteLeadingZeros(t *testing.T) {
 		want           []byte
 	}{
 		{[]byte("000022"), []byte("22")},
+		{[]byte("00022"), []byte("22")},
+		{[]byte("0022"), []byte("22")},
+		{[]byte("022"), []byte("22")},
 		{[]byte("0000"), []byte("0")},
+		{[]byte("22"), []byte("22")},
 	}
 	for _, tt := range tests {
 		t.Run(string(tt.numberAsString), func(t *testing.T) {

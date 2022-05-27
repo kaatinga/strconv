164ac1b19718bbf92d8d7f38bc76b6a5bb98858a
Mikhail Gunkov <painter@3lines.club>
2022-05-27 23:20:33 +0200

    better test for 2string converters

--- to_string_converters_test.go
+++ to_string_converters_test.go
@@ -1,44 +1,27 @@
 package faststrconv
 
 import (
+	"math/rand"
+	"strconv"
 	"testing"
 )
 
 func Test2String(t *testing.T) {
-	tests := []struct {
-		result string
-		num    uint16
-	}{
-		{"0", 0},
-		{"1", 1},
-		{"9", 9},
-		{"10", 10},
-		{"22", 22},
-		{"99", 99},
-		{"100", 100},
-		{"199", 199},
-		{"222", 222},
-		{"255", 255},
-		{"1999", 1999},
-		{"9999", 9999},
-		{"10000", 10000},
-		{"55555", 55555},
-		{"12345", 12345},
-	}
-
-	// nolint:scopelint
-	for _, tt := range tests {
-		t.Run(tt.result, func(t *testing.T) {
-			gotUint16 := Uint162String(tt.num)
-			if gotUint16 != tt.result {
-				t.Errorf("Uint162String() = %v, want %v", gotUint16, tt.result)
+	var num int32
+	for i := 0; i < 1000; i++ {
+		result := strconv.Itoa(int(num))
+		t.Run(result, func(t *testing.T) {
+			gotUint16 := Uint162String(uint16(num))
+			if gotUint16 != result {
+				t.Errorf("Uint162String() = %v, want %v", gotUint16, result)
 			}
-			if tt.num < 256 {
-				gotByte := Byte2String(byte(tt.num))
-				if gotByte != tt.result {
-					t.Errorf("Byte2String() = %v, want %v", gotByte, tt.result)
+			if num < 256 {
+				gotByte := Byte2String(byte(num))
+				if gotByte != result {
+					t.Errorf("Byte2String() = %v, want %v", gotByte, result)
 				}
 			}
 		})
+		num = rand.Int31n(65535)
 	}
 }

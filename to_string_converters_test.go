04d6457667c10284a83f8d4747913f55de3f118a
Mikhail Gunkov <painter@3lines.club>
2022-05-29 14:56:27 +0200

    updated unit tests to cover uint32 cases

--- to_string_converters_test.go
+++ to_string_converters_test.go
@@ -8,20 +8,31 @@ import (
 
 func Test2String(t *testing.T) {
 	var num int32
-	for i := 0; i < 1000; i++ {
+	for i := 0; i < 5000; i++ {
 		result := strconv.Itoa(int(num))
+		var gotUint32, gotUint16, gotByte string
 		t.Run(result, func(t *testing.T) {
-			gotUint16 := Uint162String(uint16(num))
-			if gotUint16 != result {
-				t.Errorf("Uint162String() = %v, want %v", gotUint16, result)
+			gotUint32 = Uint322String(uint32(num))
+			if gotUint32 != result {
+				t.Errorf("Uint322String() = %v, want %v", gotUint32, result)
+			}
+			if num < 65535 {
+				gotUint16 = Uint162String(uint16(num))
+				if gotUint16 != result {
+					t.Errorf("Uint162String() = %v, want %v", gotUint16, result)
+				}
 			}
 			if num < 256 {
-				gotByte := Byte2String(byte(num))
+				gotByte = Byte2String(byte(num))
 				if gotByte != result {
 					t.Errorf("Byte2String() = %v, want %v", gotByte, result)
 				}
 			}
 		})
-		num = rand.Int31n(65535)
+		if num > 65535 {
+			num = rand.Int31n(1535)
+		} else {
+			num = rand.Int31()
+		}
 	}
 }

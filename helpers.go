a0fcef9819256d0fccc857fc67505b746d3116ea
Mikhail Gunkov <painter@3lines.club>
2022-05-29 22:24:34 +0200

    changed name of type parameter

--- helpers.go
+++ helpers.go
@@ -1,7 +1,7 @@
 package faststrconv
 
 // DeleteLeadingZeros removes all the leading zeros in the input string.
-func DeleteLeadingZeros[I input](input I) I {
+func DeleteLeadingZeros[I stringOrBytes](input I) I {
 	key := 0
 	for ; key < len(input); key++ {
 		if input[key] != 48 {

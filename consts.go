df39a8f16b01cd718296b5af4ac77283b592fdd0
Mikhail Gunkov <painter@3lines.club>
2022-05-29 22:23:46 +0200

    added helper constants

--- consts.go
+++ consts.go
@@ -6,4 +6,8 @@ const (
 	MaxUint32Mask  = 0xffffffff
 	DigitsMask     = 0b110000
 	ByteLengthMask = 0b11 // it allows to catch cases faster when the number is too long.
+
+	MaxUint8  = 1<<8 - 1
+	MaxUint16 = 1<<16 - 1
+	MaxUint32 = 1<<32 - 1
 )

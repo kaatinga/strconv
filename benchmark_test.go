fdcb55d596e54cafa93e6f81efa2054708890241
Michael Gunkoff <painter@3lines.club>
2025-04-30 23:21:48 +0200

    updated benchmarks

--- benchmark_test.go
+++ benchmark_test.go
@@ -69,66 +69,74 @@ import (
 func BenchmarkByte2String(b *testing.B) {
 	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
-		Byte2String(199)
-		Byte2String(0)
-		Byte2String(55)
+		_ = Byte2String(199)
+		_ = Byte2String(0)
+		_ = Byte2String(55)
 	}
 }
 
 func BenchmarkUint162String(b *testing.B) {
 	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
-		Uint162String(199)
-		Uint162String(0)
-		Uint162String(55)
-		Uint162String(64555)
+		_ = Uint162String(199)
+		_ = Uint162String(0)
+		_ = Uint162String(55)
 	}
 }
 
-func BenchmarkUint322String(b *testing.B) {
-	b.ReportAllocs()
-	for i := 0; i < b.N; i++ {
-		Uint322String(199)
-		Uint322String(0)
-		Uint322String(55)
-		Uint322String(64555)
-	}
-}
+// func BenchmarkUint2Bytes(b *testing.B) {
+// 	b.ReportAllocs()
+// 	for i := 0; i < b.N; i++ {
+// 		_ = Uint2String(199)
+// 		_ = Uint2String(0)
+// 		_ = Uint2String(55)
+// 		_ = Uint2String(64555)
+// 	}
+// }
 
-func BenchmarkUint642String(b *testing.B) {
-	b.ReportAllocs()
-	for i := 0; i < b.N; i++ {
-		Uint642String(199)
-		Uint642String(0)
-		Uint642String(55)
-		Uint642String(64555)
-	}
-}
-
-func BenchmarkUint32And642StringWithUint64(b *testing.B) {
-	uint199 := uint64(199)
-	uint0 := uint64(0)
-	uint55 := uint64(55)
-	uint64555 := uint64(64555)
-	b.ReportAllocs()
-	for i := 0; i < b.N; i++ {
-		Uint32And642String(uint199)
-		Uint32And642String(uint0)
-		Uint32And642String(uint55)
-		Uint32And642String(uint64555)
-	}
-}
-
-func BenchmarkUint32And642StringWithUint32(b *testing.B) {
-	b.ReportAllocs()
-	for i := 0; i < b.N; i++ {
-		Uint32And642String(uint32(199))
-		Uint32And642String(uint32(0))
-		Uint32And642String(uint32(55))
-		Uint32And642String(uint32(64555))
-	}
-}
-
+//	func BenchmarkUint322String(b *testing.B) {
+//		b.ReportAllocs()
+//		for i := 0; i < b.N; i++ {
+//			Uint322String(199)
+//			Uint322String(0)
+//			Uint322String(55)
+//			Uint322String(64555)
+//		}
+//	}
+//
+//	func BenchmarkUint642String(b *testing.B) {
+//		b.ReportAllocs()
+//		for i := 0; i < b.N; i++ {
+//			Uint642String(199)
+//			Uint642String(0)
+//			Uint642String(55)
+//			Uint642String(64555)
+//		}
+//	}
+//
+//	func BenchmarkUint32And642StringWithUint64(b *testing.B) {
+//		uint199 := uint64(199)
+//		uint0 := uint64(0)
+//		uint55 := uint64(55)
+//		uint64555 := uint64(64555)
+//		b.ReportAllocs()
+//		for i := 0; i < b.N; i++ {
+//			Uint32And642String(uint199)
+//			Uint32And642String(uint0)
+//			Uint32And642String(uint55)
+//			Uint32And642String(uint64555)
+//		}
+//	}
+//
+//	func BenchmarkUint32And642StringWithUint32(b *testing.B) {
+//		b.ReportAllocs()
+//		for i := 0; i < b.N; i++ {
+//			Uint32And642String(uint32(199))
+//			Uint32And642String(uint32(0))
+//			Uint32And642String(uint32(55))
+//			Uint32And642String(uint32(64555))
+//		}
+//	}
 func BenchmarkItoa(b *testing.B) {
 	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {

f46cb5e1570207edeec4fdadb23f87138569e585
Michael Gunkoff <painter@3lines.club>
2025-04-30 22:36:31 +0200

    more cases for GetUint32

--- benchmark_test.go
+++ benchmark_test.go
@@ -5,64 +5,66 @@ import (
 	"testing"
 )
 
-////nolint
-//func BenchmarkGetByte(b *testing.B) {
-//	b.ReportAllocs()
-//	for i := 0; i < b.N; i++ {
-//		GetByte("0")
-//		//goland:noinspection GoUnhandledErrorResult
-//		GetByte("255")
+// //nolint
+//	func BenchmarkGetByte(b *testing.B) {
+//		b.ReportAllocs()
+//		for i := 0; i < b.N; i++ {
+//			GetByte("0")
+//			//goland:noinspection GoUnhandledErrorResult
+//			GetByte("255")
+//		}
 //	}
-//}
 //
-//// nolint
-//func BenchmarkGetUint16(b *testing.B) {
-//	b.ReportAllocs()
-//	for i := 0; i < b.N; i++ {
-//		//goland:noinspection GoUnhandledErrorResult
-//		GetUint16("0")
-//		GetUint16("255")
+// // nolint
+//	func BenchmarkGetUint16(b *testing.B) {
+//		b.ReportAllocs()
+//		for i := 0; i < b.N; i++ {
+//			//goland:noinspection GoUnhandledErrorResult
+//			GetUint16("0")
+//			GetUint16("255")
+//		}
 //	}
-//}
 //
-//// nolint
-//func BenchmarkGetUint16DeletingLeadingZeros(b *testing.B) {
-//	b.ReportAllocs()
-//	for i := 0; i < b.N; i++ {
-//		//goland:noinspection GoUnhandledErrorResult
-//		GetUint16(DeleteLeadingZeros("0000"))
-//		GetUint16(DeleteLeadingZeros("0000255"))
+// // nolint
+//	func BenchmarkGetUint16DeletingLeadingZeros(b *testing.B) {
+//		b.ReportAllocs()
+//		for i := 0; i < b.N; i++ {
+//			//goland:noinspection GoUnhandledErrorResult
+//			GetUint16(DeleteLeadingZeros("0000"))
+//			GetUint16(DeleteLeadingZeros("0000255"))
+//		}
 //	}
-//}
 //
-//// nolint
-//func BenchmarkGetUint32(b *testing.B) {
-//	b.ReportAllocs()
-//	for i := 0; i < b.N; i++ {
-//		GetUint32("0")
-//		GetUint32("255")
-//	}
-//}
+// // nolint
+// func BenchmarkGetUint32(b *testing.B) {
+// 	b.ReportAllocs()
+// 	for i := 0; i < b.N; i++ {
+// 		GetUint32("0")
+// 		GetUint32("255")
+// 		GetUint32("2550000")
+// 		GetUint32("25500999900")
+// 	}
+// }
 //
-//// nolint
-//func BenchmarkStrvconv_Atoi(b *testing.B) {
+// // nolint
+// func BenchmarkStrvconv_Atoi(b *testing.B) {
 //	b.ReportAllocs()
 //	for i := 0; i < b.N; i++ {
 //		//goland:noinspection ALL,GoUnhandledErrorResult
 //		strconv.Atoi("0")
 //		strconv.Atoi("255")
 //	}
-//}
+// }
 //
-//// nolint
-//func BenchmarkStrvconv_AtoiDeletingLeadingZeros(b *testing.B) {
+// // nolint
+// func BenchmarkStrvconv_AtoiDeletingLeadingZeros(b *testing.B) {
 //	b.ReportAllocs()
 //	for i := 0; i < b.N; i++ {
 //		//goland:noinspection ALL,GoUnhandledErrorResult
 //		strconv.Atoi("0000")
 //		strconv.Atoi("0000255")
 //	}
-//}
+// }
 
 func BenchmarkByte2String(b *testing.B) {
 	b.ReportAllocs()

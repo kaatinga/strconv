cef5b2277cc2c72d035b1737619853e66f0dadf9
Michael Gunkoff <painter@3lines.club>
2025-04-30 23:22:31 +0200

    add Uint2String()

--- to_string_converters.go
+++ to_string_converters.go
@@ -1,26 +1,31 @@
 package faststrconv
 
-// Byte2String converts a byte number to string.
+// Byte2String converts a byte to string.
 func Byte2String(num byte) string {
 	return string(Byte2Bytes(num))
 }
 
-// Uint162String converts an uint16 number to string.
+// Uint162String converts an uint16 to string.
 func Uint162String(num uint16) string {
 	return string(Uint162Bytes(num))
 }
 
-// Uint322String converts an uint32 number to string.
+// Uint322String converts an uint32 to string.
 func Uint322String(num uint32) string {
 	return string(Uint322Bytes(num))
 }
 
-// Uint642String converts an uint64 number to string.
+// Uint642String converts an uint64 to string.
 func Uint642String(num uint64) string {
 	return string(Uint642Bytes(num))
 }
 
-// Uint32And642String converts an uint64 or uint32 number to string.
+// Uint2String converts an uint, uint32 or uint64 to string.
+func Uint2String(num uint64) string {
+	return string(Uint2Bytes(num))
+}
+
+// Uint32And642String converts an uint64 or uint32 to string.
 func Uint32And642String[UI unsigned](num UI) string {
 	return string(Uint2Bytes(num))
 }

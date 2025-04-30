036f3fb8dd2a3257bb4bf8af5ddcfcccb67ec75b
Michael Gunkoff <painter@3lines.club>
2025-04-30 22:36:45 +0200

    removed an unnecessary constant

--- consts.go
+++ consts.go
@@ -3,7 +3,6 @@ package faststrconv
 const (
 	maxByteMask    = 0xff
 	maxUint16Mask  = 0xffff
-	maxUint32Mask  = 0xffffffff
 	digitsMask     = 0b110000
 	byteLengthMask = 0b11 // it allows to catch cases faster when the number is too long.
 

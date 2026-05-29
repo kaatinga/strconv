41151057ee1827578ec73805718bf8a4124c4e6e
Michael Gunkoff <kaatinga@gmail.com>
2026-05-29 12:43:29 +0200

    update benchmaeks

--- benchmark_test.go
+++ benchmark_test.go
@@ -5,165 +5,185 @@ import (
 	"testing"
 )
 
-// //nolint
-//	func BenchmarkGetByte(b *testing.B) {
-//		b.ReportAllocs()
-//		for i := 0; i < b.N; i++ {
-//			GetByte("0")
-//			//goland:noinspection GoUnhandledErrorResult
-//			GetByte("255")
-//		}
-//	}
+// NOTE on methodology: benchmarks must read their inputs from mutable
+// package-level slices (indexed by the loop counter) and store their results in
+// package-level sinks. Passing compile-time constants lets the compiler
+// constant-fold/elide the work, and discarding results lets escape analysis
+// remove the allocations entirely - both make the numbers meaninglessly report
+// "0 ns/op, 0 B/op, 0 allocs/op".
 //
-// // nolint
-//	func BenchmarkGetUint16(b *testing.B) {
-//		b.ReportAllocs()
-//		for i := 0; i < b.N; i++ {
-//			//goland:noinspection GoUnhandledErrorResult
-//			GetUint16("0")
-//			GetUint16("255")
-//		}
-//	}
-//
-// // nolint
-//	func BenchmarkGetUint16DeletingLeadingZeros(b *testing.B) {
-//		b.ReportAllocs()
-//		for i := 0; i < b.N; i++ {
-//			//goland:noinspection GoUnhandledErrorResult
-//			GetUint16(DeleteLeadingZeros("0000"))
-//			GetUint16(DeleteLeadingZeros("0000255"))
-//		}
-//	}
-//
-// // nolint
-// func BenchmarkGetUint32(b *testing.B) {
-// 	b.ReportAllocs()
-// 	for i := 0; i < b.N; i++ {
-// 		GetUint32("0")
-// 		GetUint32("255")
-// 		GetUint32("2550000")
-// 		GetUint32("25500999900")
-// 	}
-// }
-//
-// // nolint
-// func BenchmarkStrvconv_Atoi(b *testing.B) {
-//	b.ReportAllocs()
-//	for i := 0; i < b.N; i++ {
-//		//goland:noinspection ALL,GoUnhandledErrorResult
-//		strconv.Atoi("0")
-//		strconv.Atoi("255")
-//	}
-// }
-//
-// // nolint
-// func BenchmarkStrvconv_AtoiDeletingLeadingZeros(b *testing.B) {
-//	b.ReportAllocs()
-//	for i := 0; i < b.N; i++ {
-//		//goland:noinspection ALL,GoUnhandledErrorResult
-//		strconv.Atoi("0000")
-//		strconv.Atoi("0000255")
-//	}
-// }
+// Every faststrconv benchmark is paired with the closest standard-library
+// strconv equivalent over the same inputs so the two can be compared directly.
+var (
+	sinkString string
+	sinkBytes  []byte
+	sinkU16    uint16
+	sinkByte   byte
+	sinkU32    uint32
+
+	inBytes   = []byte{199, 0, 55, 255}
+	inU16     = []uint16{199, 0, 55, 64555}
+	inU32     = []uint32{199, 0, 2550000, 4294967295}
+	inU64     = []uint64{199, 0, 2550000, 18446744073709551615}
+	inParseB  = []string{"0", "99", "255", "7"}
+	inParse16 = []string{"0", "255", "16000", "64555"}
+	inParse32 = []string{"0", "255", "2550000", "4294967295"}
+)
+
+// ---------------------------------------------------------------------------
+// string -> number (parsing)   vs   strconv.ParseUint
+// ---------------------------------------------------------------------------
+
+func BenchmarkGetByte(b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		sinkByte, _ = GetByte(inParseB[i&3])
+	}
+}
+
+func BenchmarkParseUint_byte(b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		v, _ := strconv.ParseUint(inParseB[i&3], 10, 8)
+		sinkByte = byte(v)
+	}
+}
+
+func BenchmarkGetUint16(b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		sinkU16, _ = GetUint16(inParse16[i&3])
+	}
+}
+
+func BenchmarkParseUint_uint16(b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		v, _ := strconv.ParseUint(inParse16[i&3], 10, 16)
+		sinkU16 = uint16(v)
+	}
+}
+
+func BenchmarkGetUint32(b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		sinkU32, _ = GetUint32(inParse32[i&3])
+	}
+}
+
+func BenchmarkParseUint_uint32(b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		v, _ := strconv.ParseUint(inParse32[i&3], 10, 32)
+		sinkU32 = uint32(v)
+	}
+}
+
+func BenchmarkAtoi(b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		v, _ := strconv.Atoi(inParse32[i&3])
+		sinkU32 = uint32(v)
+	}
+}
+
+// ---------------------------------------------------------------------------
+// number -> string   vs   strconv.FormatUint / strconv.Itoa
+// ---------------------------------------------------------------------------
 
 func BenchmarkByte2String(b *testing.B) {
 	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
-		_ = Byte2String(199)
-		_ = Byte2String(0)
-		_ = Byte2String(55)
-		_ = Byte2String(255)
+		sinkString = Byte2String(inBytes[i&3])
 	}
 }
 
 func BenchmarkUint162String(b *testing.B) {
 	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
-		_ = Uint162String(199)
-		_ = Uint162String(0)
-		_ = Uint162String(55)
-		_ = Uint162String(64555)
+		sinkString = Uint162String(inU16[i&3])
 	}
 }
-
-// func BenchmarkByte2Bytes(b *testing.B) {
-// 	b.ReportAllocs()
-// 	for i := 0; i < b.N; i++ {
-// 		_ = Byte2Bytes(199)
-// 		_ = Byte2Bytes(0)
-// 		_ = Byte2Bytes(55)
-// 	}
-// }
-// func BenchmarkUint162Bytes(b *testing.B) {
-// 	b.ReportAllocs()
-// 	for i := 0; i < b.N; i++ {
-// 		_ = Uint162Bytes(199)
-// 		_ = Uint162Bytes(0)
-// 		_ = Uint162Bytes(55)
-// 		_ = Uint162Bytes(64555)
-// 	}
-// }
-
-// func BenchmarkUint2Bytes(b *testing.B) {
-// 	b.ReportAllocs()
-// 	for i := 0; i < b.N; i++ {
-// 		_ = Uint2String(199)
-// 		_ = Uint2String(0)
-// 		_ = Uint2String(55)
-// 		_ = Uint2String(64555)
-// 	}
-// }
 
 func BenchmarkUint322String(b *testing.B) {
 	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
-		Uint322String(199)
-		Uint322String(0)
-		Uint322String(55)
-		Uint322String(64555)
+		sinkString = Uint322String(inU32[i&3])
 	}
 }
 
 func BenchmarkUint642String(b *testing.B) {
 	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
-		Uint642String(199)
-		Uint642String(0)
-		Uint642String(55)
-		Uint642String(64555)
+		sinkString = Uint642String(inU64[i&3])
 	}
 }
 
-func BenchmarkUint32And642StringWithUint64(b *testing.B) {
-	uint199 := uint64(199)
-	uint0 := uint64(0)
-	uint55 := uint64(55)
-	uint64555 := uint64(64555)
+func BenchmarkUint32And642String(b *testing.B) {
 	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
-		Uint32And642String(uint199)
-		Uint32And642String(uint0)
-		Uint32And642String(uint55)
-		Uint32And642String(uint64555)
+		sinkString = Uint32And642String(inU64[i&3])
 	}
 }
 
-func BenchmarkUint32And642StringWithUint32(b *testing.B) {
+func BenchmarkFormatUint(b *testing.B) {
 	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
-		Uint32And642String(uint32(199))
-		Uint32And642String(uint32(0))
-		Uint32And642String(uint32(55))
-		Uint32And642String(uint32(64555))
+		sinkString = strconv.FormatUint(inU64[i&3], 10)
 	}
 }
 
 func BenchmarkItoa(b *testing.B) {
 	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
-		strconv.Itoa(199)
-		strconv.Itoa(0)
-		strconv.Itoa(55)
-		strconv.Itoa(64555)
+		sinkString = strconv.Itoa(int(inU32[i&3]))
+	}
+}
+
+// ---------------------------------------------------------------------------
+// number -> []byte   vs   strconv.AppendUint
+// ---------------------------------------------------------------------------
+
+func BenchmarkByte2Bytes(b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		sinkBytes = Byte2Bytes(inBytes[i&3])
+	}
+}
+
+func BenchmarkUint162Bytes(b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		sinkBytes = Uint162Bytes(inU16[i&3])
+	}
+}
+
+func BenchmarkUint322Bytes(b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		sinkBytes = Uint322Bytes(inU32[i&3])
+	}
+}
+
+func BenchmarkUint642Bytes(b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		sinkBytes = Uint642Bytes(inU64[i&3])
+	}
+}
+
+func BenchmarkUint2Bytes(b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		sinkBytes = Uint2Bytes(inU64[i&3])
+	}
+}
+
+// AppendUint into a fresh, exactly-sized slice is the closest strconv analogue
+// to the Uint*2Bytes helpers (which also return a fresh, exactly-sized slice).
+func BenchmarkAppendUint(b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		sinkBytes = strconv.AppendUint(nil, inU64[i&3], 10)
 	}
 }
